dfdd0cdaebcc8c97105c8deeee142ef46d8e3f93
Viers <krusnik.viers@gmail.com>
2022-09-02 14:34:14 +0200

    Fix stored settings tuple

--- app/data/storage/settings.py
+++ app/data/storage/settings.py
@@ -5,14 +5,14 @@ from PySide2.QtCore import QSettings
 
 from app.info import PROJECT_NAME, PUBLISHER_NAME
 
-_StoredSettingMeta = namedtuple('StoredSettingMeta', 'type default_value')
+_StoredSettingMeta = namedtuple('StoredSettingMeta', 'stored_name type default_value')
 
 
 # Add new values to be stored in this enum.
 class StoredSettings(Enum):
-    LAST_PROJECT_FILE_PATH = _StoredSettingMeta(str, '')
-    LAST_ANKI_FILE_PATH = _StoredSettingMeta(str, '')
-    IMPORT_ON_STARTUP = _StoredSettingMeta(bool, True)
+    LAST_PROJECT_FILE_PATH = _StoredSettingMeta('last_json_path', str, '')
+    LAST_ANKI_FILE_PATH = _StoredSettingMeta('last_apkg_path', str, '')
+    IMPORT_ON_STARTUP = _StoredSettingMeta('startup_autoimport', bool, True)
 
 
 class Settings:
@@ -20,10 +20,10 @@ class Settings:
     def _qt_adapter():
         return QSettings(PUBLISHER_NAME, PROJECT_NAME)
 
-    @staticmethod
-    def set(key: StoredSettings, value):
-        Settings._qt_adapter().setValue(key.name, value)
+    @classmethod
+    def set(cls, key: StoredSettings, value):
+        cls._qt_adapter().setValue(key.value.stored_name, value)
 
-    @staticmethod
-    def get(key: StoredSettings):
-        return Settings._qt_adapter().value(key.name, defaultValue=key.value.default_value, type=key.value.type)
+    @classmethod
+    def get(cls, key: StoredSettings):
+        return cls._qt_adapter().value(key.value.stored_name, defaultValue=key.value.default_value, type=key.value.type)
